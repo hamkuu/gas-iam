221f1de1dfa89fb32c84c834af9c7a4a63d58a7b
Hiro Shaw <hamkuu@gmail.com>
2026-05-25 13:39:57 +0900

    define user model

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 uv run python manage.py migrate
+uv run python manage.py loaddata accounts/fixtures/users.json
 uv run python manage.py loaddata accounts/fixtures/prefectures.json
-uv run python manage.py loaddata fixtures/users.json
 
 uv run python manage.py runserver 0.0.0.0:8000
