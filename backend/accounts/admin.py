9405b1416cba3d1f5c2077abd7c834d42b828cf7
Hiro Shaw <hamkuu@gmail.com>
2026-05-25 13:15:13 +0900

    prefecture model

--- backend/accounts/admin.py
+++ backend/accounts/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from accounts.models import Prefecture
+
+
+@admin.register(Prefecture)
+class PrefectureAdmin(admin.ModelAdmin):
+    list_display = ["code", "name", "name_en"]
+    ordering = ["code"]
--- backend/accounts/models.py
+++ backend/accounts/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Prefecture(models.Model):
+    code = models.PositiveSmallIntegerField(unique=True)
+    name = models.CharField(max_length=10)
+    name_en = models.CharField(max_length=50)
+
+    class Meta:
+        ordering = ["code"]
+
+    def __str__(self):
+        return self.name
