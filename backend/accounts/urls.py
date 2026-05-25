b2dd4ddf36aa3372408fcd1bbc4c62c3451660bb
Hiro Shaw <hamkuu@gmail.com>
2026-05-25 17:24:14 +0900

    add login page

--- backend/accounts/urls.py
+++ backend/accounts/urls.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
 
 from . import views
@@ -5,6 +6,8 @@ from . import views
 app_name = "accounts"
 
 urlpatterns = [
+    path("", LoginView.as_view(template_name="accounts/login.html"), name="login"),
     path("register/", views.register, name="register"),
     path("profile/", views.profile, name="profile"),
+    path("logout/", LogoutView.as_view(next_page="accounts:login"), name="logout"),
 ]
