94c9429c2bb28c29dd08ef7672799bc0b335bc96
Hiro Shaw <hamkuu@gmail.com>
2026-05-26 14:52:37 +0900

    cleanup

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-uv run python manage.py migrate
-uv run python manage.py loaddata accounts/fixtures/users.json
-uv run python manage.py loaddata accounts/fixtures/prefectures.json
+python manage.py migrate
+python manage.py loaddata accounts/fixtures/users.json
+python manage.py loaddata accounts/fixtures/prefectures.json
 
-uv run python manage.py runserver 0.0.0.0:8000
+python manage.py runserver 0.0.0.0:8000
