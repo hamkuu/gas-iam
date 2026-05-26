56d5ac89af1ddb902667d8caf87ad6f54b2f9d03
Hiro Shaw <hamkuu@gmail.com>
2026-05-26 15:18:24 +0900

    Introduce DRF and JWT

--- backend/gas_iam/settings.py
+++ backend/gas_iam/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "rest_framework",
     "accounts",
 ]
 
@@ -49,6 +50,15 @@ MIDDLEWARE = [
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
+
+REST_FRAMEWORK = {
+    "DEFAULT_AUTHENTICATION_CLASSES": [
+        "rest_framework_simplejwt.authentication.JWTAuthentication",
+    ],
+    "DEFAULT_PERMISSION_CLASSES": [
+        "rest_framework.permissions.IsAuthenticated",
+    ],
+}
 
 ROOT_URLCONF = "gas_iam.urls"
 
--- backend/gas_iam/urls.py
+++ backend/gas_iam/urls.py
@@ -19,8 +19,12 @@ from django.contrib import admin
 from django.shortcuts import redirect
 from django.urls import include, path
 
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+
 urlpatterns = [
     path("", lambda request: redirect("accounts:login"), name="index"),
     path("admin/", admin.site.urls),
     path("accounts/", include("accounts.urls")),
+    path("api/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
 ]
