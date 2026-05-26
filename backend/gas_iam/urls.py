346653e246bf11cb2a6a35bb2bdb74da160361bd
Hiro Shaw <hamkuu@gmail.com>
2026-05-26 16:01:50 +0900

    remove duplicate APIs

--- backend/gas_iam/urls.py
+++ backend/gas_iam/urls.py
@@ -19,13 +19,9 @@ from django.contrib import admin
 from django.shortcuts import redirect
 from django.urls import include, path
 
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
-
 urlpatterns = [
     path("", lambda request: redirect("accounts:login"), name="index"),
     path("admin/", admin.site.urls),
     path("accounts/", include("accounts.urls")),
-    path("api/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
-    path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
     path("api/auth/", include("dj_rest_auth.urls")),
 ]
