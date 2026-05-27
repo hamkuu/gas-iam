b0ef11fedbac25b57fd98f424bb639ee68d39333
Hiro Shaw <hamkuu@gmail.com>
2026-05-27 20:32:47 +0900

    deploy backend image

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -5,5 +5,5 @@ python manage.py migrate
 if [ "$DEBUG" = "1" ]; then
   python manage.py runserver 0.0.0.0:8000
 else
-  exec gunicorn gas_iam.wsgi:application --bind 0.0.0.0:8000 --workers 2
+  exec gunicorn gas_iam.wsgi:application --bind 0.0.0.0:${PORT:-8000} --workers 2
 fi
